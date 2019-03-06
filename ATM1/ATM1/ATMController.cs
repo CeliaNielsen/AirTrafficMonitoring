4e379044d6e63e8292d19f3c67e84a3daa7883c7
Celia <au582514@post.au.dk>
2019-03-06 11:34:27 +0100

    ATM controller tilføjer forbindelse til ITransponder lavet Klassen FilterAirspace oprettet

---
+++ ATM1/ATM1/ATMController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace ATM1
+{
+    class ATMController
+    {
+       
+        public List<string> currentSignal { get; set; }
+
+        public ATMController(ITransponderReceiver transponderReceiver)
+        {
+            transponderReceiver.TransponderDataReady += HandleTransponderSignalEvent; // ATM forbindes til ITransponderReceiver
+        }
+
+        private void HandleTransponderSignalEvent(object sender, RawTransponderDataEventArgs e)
+        {
+            currentSignal = e.TransponderData;
+        }
+
+    }
+}
