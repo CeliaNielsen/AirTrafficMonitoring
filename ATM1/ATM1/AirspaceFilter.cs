cefa9374aff37d22b1ca2670479ebf2c58092323
Celia <au582514@post.au.dk>
2019-03-13 10:49:29 +0100

    oprettelse af klasserne: Track, IFilter, AirspaceFilter

---
+++ ATM1/ATM1/AirspaceFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM1
+{
+    class AirspaceFilter : IFilter
+    {
+        // three-dimensional shape -> defined by a two-dimensional -> both sides being 8000 meter
+
+        // x-koordinater
+        // y-koordinater
+        private Track _track;
+        private ICalculate _calculate = new ICalculate(); 
+       
+
+        private bool inAirspace;
+
+        public void CheckAirspace()
+        {
+            if (expr)
+            {
+                inAirspace = true;
+
+                _calculate.CalculateTrack(_track);
+            }
+            
+        }
+    }
+}
---
+++ ATM1/ATM1/Track.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM1
+{
+    class Track
+    {
+        public string Tag { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Altitude { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Track(string tag, double x, double y, double altitude, DateTime timeStamp)
+        {
+            Tag = tag;
+            X = x;
+            Y = y;
+            Altitude = altitude;
+            TimeStamp = timeStamp;
+        }
+    }
+}
