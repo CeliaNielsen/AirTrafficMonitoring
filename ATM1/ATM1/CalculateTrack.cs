4ba057675f02654eded4dce1b36ff02e3b4d6bcf
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2019-03-19 08:09:50 +0100

    CalculateSpeed( ): Konvertering fra TimeSpan til Double

--- ATM1/ATM1/CalculateTrack.cs
+++ ATM1/ATM1/CalculateTrack.cs
@@ -13,7 +13,6 @@ namespace ATM1
         //private IConditions _conditions;
         private IFilter _filter;
         
-        
         //DTO'en
         private Track _dtoTrack;
 
@@ -26,30 +25,30 @@ namespace ATM1
 
         public double CalculateSpeed()
         {
-            double speed; 
-            
+            double speed = 0;
             foreach (var airplane in ) //har vi en liste med de fly som er i airspace? 
             {
                 DateTime t1 = _dtoTrack.TimeStamp;
                 double x1 = _dtoTrack.X;
                 double y1 = _dtoTrack.Y;
 
-                if (_filter.CheckAirspace() == true)
+                if (_filter.CheckAirspace() == true) //skal tjekke om flyet STADIG er i airspace 
                 {
                     DateTime t2 = _dtoTrack.TimeStamp;
                     double x2 = _dtoTrack.X;
                     double y2 = _dtoTrack.Y;
         
-                    //tidsforskellen 
+                    //Tidsforskellen 
                     TimeSpan td = t2-t1; 
 
                     //Afstanden er x og y koordinater fra første timestamp til andet timestamp (afstandsformlen)
                     double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-                        //Hastigheden fås ved tidsforskellen delt med afstanden
-                        speed = distance / td;
+
+                    //Hastigheden fås ved tidsforskellen delt med afstanden
+                    double speed1 = distance / Convert.ToDouble(td);
+                    speed = speed1;
                 }
             }
-
             return speed;
         }
 
