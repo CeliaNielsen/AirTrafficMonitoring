622e1d1cff179b0841c17774ee79313e052e7f61
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2019-03-20 10:17:40 +0100

    Calculate

--- ATM1/ATM1/CalculateTrack.cs
+++ ATM1/ATM1/CalculateTrack.cs
@@ -7,55 +7,47 @@ using System.Threading.Tasks;
 
 namespace ATM1
 {
-    public class CalculateTrack/*:ICalculate*/
+    public class CalculateTrack:ICalculate
     {
-        //Skal have forbindelse til IConditions og IFilter 
-        //private IConditions _conditions;
         private IFilter _filter;
-        
-        //DTO'en
-        private Track _dtoTrack;
+        private List<Track> _trackList;
+
 
         public void CalculateCompassCourse()
         {
            
-
-
         }
-
-        public double CalculateSpeed()
+        public double CalculateSpeed(List<Track> trackList)
         {
             double speed = 0;
-            foreach (var airplane in ) //har vi en liste med de fly som er i airspace? 
+            foreach (var airplane in trackList) //har vi en liste med de fly som er i airspace? 
             {
-                DateTime t1 = _dtoTrack.TimeStamp;
-                double x1 = _dtoTrack.X;
-                double y1 = _dtoTrack.Y;
+                //DateTime t1 = _dtoTrack.TimeStamp;
+                //double x1 = _dtoTrack.X;
+                //double y1 = _dtoTrack.Y;
+                //string tag1 = _dtoTrack.Tag; 
 
-                if (_filter.CheckAirspace() == true) //skal tjekke om flyet STADIG er i airspace 
+                if (trackList.tag == tag1 && _trackList.X != x1 && _trackList.Y != y1) //når samme tag opstår i listen, så den udregne  
                 {
-                    DateTime t2 = _dtoTrack.TimeStamp;
-                    double x2 = _dtoTrack.X;
-                    double y2 = _dtoTrack.Y;
+                    //Hvis vi siger tag1 = tag, så tager den vel bare samme track? vi skal finde en måde at sikre på at det er en anden placering/track
+
+
+                    //DateTime t2 = _dtoTrack.TimeStamp;
+                    //double x2 = _dtoTrack.X;
+                    //double y2 = _dtoTrack.Y;
         
                     //Tidsforskellen 
-                    TimeSpan td = t2-t1; 
+                    //TimeSpan td = t2-t1; 
 
                     //Afstanden er x og y koordinater fra første timestamp til andet timestamp (afstandsformlen)
-                    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                    //double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
 
                     //Hastigheden fås ved tidsforskellen delt med afstanden
-                    double speed1 = distance / Convert.ToDouble(td);
-                    speed = speed1;
+                    //double speed1 = distance / Convert.ToDouble(td);
+                    //speed = speed1;
                 }
             }
             return speed;
-        }
-
-        //Metode der indeholder følgende tre værdier: Trackdata, Speed, CompassCourse
-        public void CalculatedValues()
-        {
-
         }
     }
 }
