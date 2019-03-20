293b34b206c8848167228939dc510e1943b8bf74
Jenny Tran <jenny.thuy.dung.tran@post.au.dk>
2019-03-20 15:09:26 +0100

    UnitTeste CalculateTrack

--- ATM1/ATM.UnitTest/CalculateTrackUnitTest.cs
+++ ATM1/ATM.UnitTest/CalculateTrackUnitTest.cs
@@ -13,10 +13,14 @@ namespace ATM.UnitTest
     class CalculateTrackUnitTest
     {
         private CalculateTrack _uut;
+        private ATMController _atmController;
 
         [SetUp]
         public void SetUp()
         {
+            _uut = new CalculateTrack();
+            _atmController = Substitute.For<ATMController>();
+
             //CalculateCompass()
 
             //CalculateSpeed()
@@ -24,6 +28,17 @@ namespace ATM.UnitTest
             //to positioner med (x og y) og sætter timestamp --> udregn afstand 
             //Vælg testdata så du ved hvad output giver! 
             //Prøv også med fortegn 
+        }
+
+        [Test]
+        public void CalculateTrack_calculateSpeed_returnResult()
+        {
+            List<Track> currentList = new List<Track>();
+            currentList.Add(new Track("ATR423", 3, 4, 100, DateTime.Today, false));
+                
+
+
+            Assert.That(_uut.CalculateSpeed(currentList)[7], Is.EqualTo(5));
         }
 
     }
