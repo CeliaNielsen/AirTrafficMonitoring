e7ad4f9b0e95248771f4252c9a877d4a014338a1
Celia <au582514@post.au.dk>
2019-03-26 13:54:16 +0100

    ændret kode i CalculateTrack

--- ATM1/ATM1/CalculateTrack.cs
+++ ATM1/ATM1/CalculateTrack.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 
 namespace ATM1
 {
-    public class CalculateTrack:ICalculate
+    public class CalculateTrack : ICalculate
     {
         public List<Track> _updatedTrackList { get; private set; }
         private List<Track> _oldTrackList;
         private ITrackFormat _trackFormat;
-  
+
         private object _lock = new object();
 
         public CalculateTrack()
         {
-            _oldTrackList= new List<Track>();
+            _oldTrackList = new List<Track>();
             _updatedTrackList = new List<Track>();
             _trackFormat = new TrackFormat();
         }
         public void CalculateCompassCourse(List<Track> trackList)
-        { 
+        {
             foreach (var airplane in trackList)
             {
                 if (airplane.X > 0 && airplane.Y > 0)
@@ -55,46 +55,50 @@ namespace ATM1
                 }
                 else
                 {
-                    airplane.CompassCourse = "West"; 
-                }        
+                    airplane.CompassCourse = "West";
+                }
             }
             _updatedTrackList = trackList;
         }
 
         public void CalculateSpeed(List<Track> trackList)
         {
-                foreach (var airplane in trackList)
+            List<Track> lokalTrackList = new List<Track>(trackList);
+
+            foreach (var airplane in lokalTrackList)
+            {
+                if (airplane.InAirSpace == true)
                 {
-                    if (airplane.InAirSpace == true)
+                    foreach (var oldairplane in _oldTrackList)
                     {
-                        foreach (var oldairplane in _oldTrackList)
+
+                        if (airplane.Tag == oldairplane.Tag && airplane.X != oldairplane.X ||
+                            airplane.Y != oldairplane.Y) //når samme tag opstår i listen, så den udregne  
                         {
+                            //Tidsforskellen
+                            double td = (airplane.TimeStamp - oldairplane.TimeStamp).Hours;
 
-                            if (airplane.Tag == oldairplane.Tag && airplane.X != oldairplane.X ||
-                                airplane.Y != oldairplane.Y) //når samme tag opstår i listen, så den udregne  
-                            {
-                                //Tidsforskellen
-                                double td = (airplane.TimeStamp - oldairplane.TimeStamp).Hours;
+                            //Afstanden er x og y koordinater fra første timestamp til andet timestamp(afstandsformlen)
+                            double distance = Math.Sqrt(Math.Pow((airplane.X - oldairplane.X), 2) +
+                                                        Math.Pow((airplane.Y - oldairplane.Y), 2));
 
-                                //Afstanden er x og y koordinater fra første timestamp til andet timestamp(afstandsformlen)
-                                double distance = Math.Sqrt(Math.Pow((airplane.X - oldairplane.X), 2) +
-                                                            Math.Pow((airplane.Y - oldairplane.Y), 2));
-
-                                //Hastigheden fås ved tidsforskellen delt med afstanden
-                                double speed1 = distance / td;
-                                airplane.Speed = speed1;
-                            }
+                            //Hastigheden fås ved tidsforskellen delt med afstanden
+                            double speed1 = distance / td;
+                            airplane.Speed = speed1;
                         }
-
-                        _oldTrackList.Add(airplane);
-
-                        _updatedTrackList = trackList;
-                        _oldTrackList = trackList;
-                        CalculateCompassCourse(trackList);
                     }
-            } 
+
+                    // _oldTrackList.Add(airplane);}}
+                    
+                }
+                
+                
+            }
+            _oldTrackList = trackList;
+            _updatedTrackList = trackList;
+            CalculateCompassCourse(trackList);
             Update(_updatedTrackList);
-            
+
         }
 
         public void Update(List<Track> trackList)
