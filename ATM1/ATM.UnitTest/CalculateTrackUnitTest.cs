5bef18e60f6712cad079f7254d37a31776684a7a
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2019-04-11 12:46:36 +0200

    CalculateTrackUnitTest

--- ATM1/ATM.UnitTest/CalculateTrackUnitTest.cs
+++ ATM1/ATM.UnitTest/CalculateTrackUnitTest.cs
@@ -26,7 +26,7 @@ namespace ATM.UnitTest
         [Test]
         public void CalculateTrack_CalculateSpeed_returnResult()
         {
-            DateTime time1 = new DateTime(2019,09,11,09,10,00);
+            DateTime time1 = new DateTime(2019, 09, 11, 09, 10, 00);
             DateTime time2 = new DateTime(2019, 09, 11, 10, 10, 00);
             List<Track> currentList = new List<Track>();
             currentList.Add(new Track("ATR423", 0, 0, 100, time1, true, null, 0));
@@ -34,10 +34,10 @@ namespace ATM.UnitTest
             currentList.Add(new Track("BTR423", 3, 4, 100, time2, true, null, 0));
             _uut.CalculateSpeed(currentList);
             //5/1 = 5 
-        
-           // Assert.That(_uut._updatedTrackList[0].Speed, Is.EqualTo(5));
-           Assert.That(_trackFormat.)
 
+            // Assert.That(_uut._updatedTrackList[0].Speed, Is.EqualTo(5));
+            _trackFormat.Received().Format(Arg.Is<List<Track>>(str => str.Contains("ATR423", 3, 4, 100, time2, true, null, 0)));
+          
         }
 
         [Test]
@@ -46,7 +46,7 @@ namespace ATM.UnitTest
             DateTime time1 = new DateTime(2019, 09, 11, 10, 10, 00);
             DateTime time2 = new DateTime(2019, 09, 11, 9, 10, 00);
             List<Track> currentList = new List<Track>();
-            currentList.Add(new Track("ATR423",0,2,100,time1,true,null,0));
+            currentList.Add(new Track("ATR423", 0, 2, 100, time1, true, null, 0));
             _uut.CalculateCompassCourse(currentList);
 
             Assert.That(_uut._updatedTrackList[0].CompassCourse, Is.EqualTo("North"));
@@ -59,7 +59,7 @@ namespace ATM.UnitTest
             DateTime time2 = new DateTime(2019, 09, 11, 10, 10, 00);
             List<Track> currentList = new List<Track>();
             currentList.Add(new Track("ATR423", 0, 0, 100, time1, true, null, 0));
-            
+
             _uut.Update(currentList);
             _trackFormat.Received(1).UpdatePrint(new List<Track>());
         }
